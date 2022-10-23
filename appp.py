b61031656c9c1374721918e0d024fd36cda99e66
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-10-23 14:22:59 +0530

    update appp.py

--- appp.py
+++ appp.py
@@ -1,1 +1,13 @@
-import 
+import os 
+from flask import Flask, request 
+ 
+
+
+server = Flask(__name__)
+
+@server.route('/')
+def getMessage(): 
+ return "okay", 200
+ 
+if __name__ == "__main__": 
+  server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
